9a8008663c250186ccc75d4187bf13a0fb89294b
Su Zhou <zsmargaret@gmail.com>
2018-11-29 15:29:22 -0800

    sz - add image model and test and add image url validation

---
+++ app/models/image.rb
@@ -0,0 +1,4 @@
+class Image < ApplicationRecord
+  URL_REGEX = %r{(https?:\/\/.*\.(?:png|jpg|gif|tif))}i
+  validates :image_url, format: { with: URL_REGEX, message: 'is invalid URL for image' }
+end
---
+++ test/models/image_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class ImageTest < ActiveSupport::TestCase
+  def test_image__valid
+    image = Image.new(image_url: 'http://fake.jpg')
+    assert_predicate image, :valid?
+  end
+
+  def test_image__invalid
+    image = Image.new(image_url: 'fake.jpg')
+    refute_predicate image, :valid?
+  end
+end
