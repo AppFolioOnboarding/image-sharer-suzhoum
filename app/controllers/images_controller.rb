5a70c2a525e6edc45e121cd418e11c6198287a2e
Su Zhou <zsmargaret@gmail.com>
2018-11-30 09:32:53 -0800

    sz - add create new image and test

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,4 +2,20 @@ class ImagesController < ApplicationController
   def new
     @image = Image.new
   end
+
+  def create
+    @image = Image.new(image_params)
+
+    if @image.save
+      redirect_to image_path(@image)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:image_url)
+  end
 end
--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -8,4 +8,26 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
     assert_select 'h1', text: 'New Image'
     assert_select '#error_explanation', count: 0
   end
+
+  def test_create__success
+    assert_difference 'Image.count', 1 do
+      post images_path, params: { image: { image_url: 'https://foo.png' } }
+    end
+
+    assert_redirected_to image_path(Image.find_by(image_url: 'https://foo.png'))
+    follow_redirect!
+    assert_select 'img' do
+      assert_select '[src=?]', 'https://foo.png'
+    end
+  end
+
+  def test_create__fail
+    assert_no_difference 'Image.count' do
+      post images_path, params: { image: { image_url: 'https://foo.invalid' } }
+    end
+
+    assert_response :ok
+    assert_select 'h1', text: 'New Image'
+    assert_select '#error_explanation', count: 1
+  end
 end
