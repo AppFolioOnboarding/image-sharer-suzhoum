dbd279aff477c98310a315bd92bdd4ffc4c74a07
Su Zhou <zsmargaret@gmail.com>
2018-12-04 12:06:12 -0800

    sz - add db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_11_28_184349) do
+
+  create_table "images", force: :cascade do |t|
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
