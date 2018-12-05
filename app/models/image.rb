1e3d8e8e1331265544c9c44903ffe7de9d1c7110
Su Zhou <zsmargaret@gmail.com>
2018-12-04 23:35:44 -0800

    sz - add acts_as_taggable_on in model and test

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,5 @@
 class Image < ApplicationRecord
   URL_REGEX = %r{(https?:\/\/.*\.(?:png|jpg|gif|tif))}i
   validates :image_url, format: { with: URL_REGEX, message: 'is invalid URL for image' }
+  acts_as_taggable_on :tags
 end
